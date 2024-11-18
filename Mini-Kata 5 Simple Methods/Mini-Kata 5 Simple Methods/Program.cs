0f534d37fe5e1a40bd4ac1c1ed4a21abf8e03db6
Jama-student <jamafahad097@gmail.com>
2024-11-18 22:23:49 +0100

    Add files via upload

---
+++ Mini-Kata 5 Simple Methods/Mini-Kata 5 Simple Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Mini_Kata_5_Simple_Methods
+{
+    using System;
+
+    class Program
+    {
+        // Method for Attack
+        static void Attack(int damage)
+        {
+            Console.WriteLine($"Player dealt {damage} damage!");
+        }
+
+        // Method for Heal
+        static void Heal(int healAmount)
+        {
+            Console.WriteLine($"Player healed {healAmount} health points!");
+        }
+
+        static void Main(string[] args)
+        {
+            // Call the Attack method with a sample value
+            Attack(15);
+
+            // Call the Heal method with a sample value
+            Heal(10);
+        }
+    }
+
+}
