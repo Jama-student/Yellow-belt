116eee7885bda3b4de6c309005acdfe77bad2d27
Jama-student <jamafahad097@gmail.com>
2024-11-20 10:23:18 +0100

    Update Program.cs

--- Mini-Kata 5 Simple Methods/Mini-Kata 5 Simple Methods/Program.cs
+++ Mini-Kata 5 Simple Methods/Mini-Kata 5 Simple Methods/Program.cs
@@ -4,13 +4,13 @@
 
     class Program
     {
-        // Method for Attack
+        
         static void Attack(int damage)
         {
             Console.WriteLine($"Player dealt {damage} damage!");
         }
 
-        // Method for Heal
+        /
         static void Heal(int healAmount)
         {
             Console.WriteLine($"Player healed {healAmount} health points!");
@@ -18,10 +18,10 @@
 
         static void Main(string[] args)
         {
-            // Call the Attack method with a sample value
+            
             Attack(15);
 
-            // Call the Heal method with a sample value
+            
             Heal(10);
         }
     }
